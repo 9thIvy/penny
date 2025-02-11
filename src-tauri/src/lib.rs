14f5053d91a8fa800db736bb9141630f4a6e0fa4
9thIvy <mythril-pops@protonmail.com>
2025-02-11 15:55:20 -0800

    Removed commented code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,13 +6,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
-        .invoke_handler(tauri::generate_handler![
-            // commands::load_file,
-            // commands::save_file,
-            // commands::list_files,
-            // commands::get_systems,
-            // commands::get_yags,
-        ])
+        .invoke_handler(tauri::generate_handler![])
         //ignore error for no OUT_DIR
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
